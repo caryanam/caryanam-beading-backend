0c4174bd0ff0b02a437c57f1fc995799a8a6f675
pratiksha-on-git <pratikshashitole@caryanamindia.in>
2026-08-12 14:23:04 +0530

    changes

--- src/main/java/com/bidding/controller/InspectorInspectionController.java
+++ src/main/java/com/bidding/controller/InspectorInspectionController.java
@@ -25,6 +25,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
